32aabbea1f8b6978ffbf9e7d8294c74af7fdede2
askpravin <askpravinjadhav@gmail.com>
2025-02-13 23:42:54 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Hypernet Networks</title>
+</head>
+
+<body>
+    <h1>Welcome to Hypernets Networks</h1>
+    <p>Click the button below to view list of hypernatives</p>
+    <a href="" class="btn">
+        Find Hypernatives!
+    </a>
+</body>
+
+</html>
